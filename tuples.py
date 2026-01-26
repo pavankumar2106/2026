12a61ee1180efca96933baff6102107367573c9c
pavan <boyalla2106@gmail.com>
2026-01-26 12:46:40 +0100

    tuples prep

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#tuples are used to store multiple values in a single variable
+thetuple=(1,2,3)
+print(thetuple)
+# tuple items are ordered and unchangeable which means we can not add or remove the items after the tuple is created
+#aloows duplicates
+#create tuple with one value
+one=(1,)
+print(type(one))
+#don't forget the ,
+one1=(1)
+print(type(one1))
